eeb094e8d56d4ce993e6e3d156493f99f73b49d1
Devantaye <devante.mika@gmail.com>
2024-08-21 17:09:05 +1200

    Initial Branch Test
    Testing out branching function + creating player class

--- Assets/player.cs
+++ Assets/player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class testplayer : MonoBehaviour
 {
+    // Player Codes goes here
+    // Testing out branching etc
+    
     // Start is called before the first frame update
     void Start()
     {
