5eec2f47a36d902d2ab4332f740d00b3b4792ac8
Devantaye <devante.mika@gmail.com>
2024-09-16 16:58:19 +1200

    Additional attack stuff
    Added more code to player_combat scripts/fixed animator for player

--- Assets/Scripts/Player_Combat.cs
+++ Assets/Scripts/Player_Combat.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class Player_Combat : MonoBehaviour
 {
-    public Animator animator;  // Reference to animator for attack animations
-    public float attackRange = 1f; // Range of attack
-    // public LayerMask enemyLayers; // Layer of enemies (Add later)
-    private testplayer playerMovement;
+    public Animator animator;            // Reference to animator for attack animations
+    public float attackRange = 1f;       // Range of attack
+    public LayerMask enemyLayers;        // Layer of enemies (for attack detection)
+    private testplayer playerMovement;   // Reference to player_movement script
+
+    //Variables for attack cooldowns (to prevent spamming
+    public float playerAttackCooldown = 1.0f; // Time in seconds between attacks
+    private float lastAttackTime = 0f;  // Time when the last attack occurred
 
     // Start is called before the first frame update
     void Start()
     {
         // Get reference to the player's movement script
         playerMovement = GetComponent<testplayer>();
+        lastAttackTime = -playerAttackCooldown;
     }
 
     // Update is called once per frame
@@ -21,15 +26,26 @@ public class Player_Combat : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Attack();
+            TryAttack();
         }
         
     }
 
+    // Checks if attack is off cooldown
+    void TryAttack()
+    {
+        if (Time.time - lastAttackTime >= playerAttackCooldown)
+        {
+            Attack();
+            lastAttackTime = Time.time; // Reset the last attack time immediately
+        }
+    }
+
+    // Attack Animation + Hit detection etc
     void Attack()
     {
         // Play attack animation
-        animator.SetTrigger("Attack"); 
+        animator.SetTrigger("Attack");
         animator.SetFloat("LastHorizontal", playerMovement.lastMovement.x);
         animator.SetFloat("LastVertical", playerMovement.lastMovement.y);
 
@@ -38,5 +54,6 @@ public class Player_Combat : MonoBehaviour
 
         //Apply damage
         //damage code here
+        animator.SetTrigger("AttackEnd");
     }
 }
